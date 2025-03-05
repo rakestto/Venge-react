77ba231e66afdbb34ae64321e88b5c21636b3cb7
Claudia Vivo Rosa <claudia@MacBook-Air-de-Claudia.local>
2025-03-05 21:32:29 +0100

    Tabla creada, con un input de range

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import Header from "./components/Header";
+
 function App() {
-  return <h1 className="bg-red-500">Hola</h1>;
+  const [cantidad, setCantidad] = useState(10000);
+
+  const MIN = 0;
+  const MAX = 10000;
+  const STEP = 100;
+
+  function handleChange(e) {
+    setCantidad(+e.target.value);
+  }
+
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+
+      <input
+        type="range"
+        className="w-full h6 bg-amber-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+      />
+
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
+        {cantidad}
+      </p>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+      Ingresa tu número de teléfono
+    </h1>
+  );
+}
+
+export default Header;
+
+// ¿Cuánto <span className="text-indigo-600">dinero</span> necesitas?
